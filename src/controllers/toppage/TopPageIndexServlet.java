920ece5888f879ebc164a243fb55f287acaf3ca0
naoki.yamada <nkymd2018@gmail.com>
2021-05-11 01:18:31 +0900

    Add LoginFilter

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -32,8 +32,9 @@ public class TopPageIndexServlet extends HttpServlet {
 			request.setAttribute("flush", request.getSession().getAttribute("flush"));
 			request.getSession().removeAttribute("flush");
 		}
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
 		rd.forward(request, response);
 	}
-
 }
+
